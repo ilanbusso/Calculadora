da4523f96684f78b8274be2da9603eda2a61028c
ILÁN URIEL BUSSO <48857364@ort>
2025-09-05 14:23:21 -0300

    final

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 
-/*function realizar(){
+function realizar(){
     let numero1 = parseFloat(document.getElementById("n1").value)
     let numero2 = parseFloat(document.getElementById("n2").value)
     let op = document.getElementById("Op").value
@@ -10,33 +10,96 @@
     else if(op == "/" && numero2 != 0) resultado.innerHTML = numero1 / numero2
     else if(op == "/" && numero2 == 0) resultado.innerHTML = "NO SE PUEDE DIVIDIR POR CERO."
     else resultado.innerHTML = numero1 * numero2
-}*/
+}
 
-function realizar2(ingresado){
-    const numero = document.getElementById('num')
-    
-    if(typeof ingresado == Number){
-        console.log("ho la")
-        let num = ingresado.toString()
-        numero.innerHTML += num
+let contenidoPantalla = "";
+let resultadoMemoria = "";
+let operadorAnterior = "";
+let numeroAnterior = "";
+
+function realizar2(valor) {
+  const pantalla = document.getElementById("num");
+
+  if (valor === 'DEL') {
+    contenidoPantalla = contenidoPantalla.substring(0, contenidoPantalla.length - 1);
+  }
+
+  else if (valor === 'M+') {
+    resultadoMemoria = contenidoPantalla;
+  }
+
+  else if (valor === 'M-') {
+    contenidoPantalla = contenidoPantalla + resultadoMemoria;
+  }
+
+  else if (valor === '+' || valor === '-' || valor === '*' || valor === '/') {
+    if (contenidoPantalla !== "") {
+      if (numeroAnterior !== "" && operadorAnterior !== "") {
+        let a = parseFloat(numeroAnterior);
+        let b = parseFloat(contenidoPantalla);
+        let resultado = "";
+
+        if (operadorAnterior === '+') {
+          resultado = a + b;
+        }
+        else if (operadorAnterior === '-') {
+          resultado = a - b;
+        }
+        else if (operadorAnterior === '*') {
+          resultado = a * b;
+        }
+        else if (operadorAnterior === '/') {
+          if (b === 0) {
+            resultado = "Error";
+          }
+          else {
+            resultado = a / b;
+          }
+        }
+
+        contenidoPantalla = resultado.toString();
+      }
+
+      numeroAnterior = contenidoPantalla;
+      operadorAnterior = valor;
+      contenidoPantalla = contenidoPantalla + valor;
     }
-    if(typeof ingresado == String){
-        if(num1 == null) num1 = numero 
-        else if (num2 == null) num2 = numero
-        else if (num1 == null && num2 == null) numero.innerHTML = 'NO SE HA INGRESADO NINGUN NUMERO'
-        else if (num1 >= 0 && num1 >= 0){/* si quiere ingresar mas de dos numeros vamos a sumar los dos numeros anteriores en 'num1' y escribir el nuevo en 'num2' y asi...*/
-            num1 = num1 + num2
-            num2 = null
-        } 
-        numero.innerHTML = ''
-        if (op == '+' || op == '-' || op == '*' || op == '/') op = ingresado
-        /*else if (op == 'M+') /* aca hay que guardar el resultado*/
-        /*else if (op == 'M') /* Traer de lka memoria el resultado guardado para poder operar */
-        else if (op == '<='){
-            num2.RemoveAt(num2.length -1)
-            numero.innerHTML = num2
-        } 
+  }
+
+  else if (valor === '=') {
+    if (numeroAnterior !== "" && operadorAnterior !== "") {
+      let a = parseFloat(numeroAnterior);
+      let textoB = contenidoPantalla.split(operadorAnterior)[1];
+      let b = parseFloat(textoB);
+      let resultado = "";
+
+      if (operadorAnterior === '+') {
+        resultado = a + b;
+      }
+      else if (operadorAnterior === '-') {
+        resultado = a - b;
+      }
+      else if (operadorAnterior === '*') {
+        resultado = a * b;
+      }
+      else if (operadorAnterior === '/') {
+        if (b === 0) {
+          resultado = "Error";
+        }
+        else {
+          resultado = a / b;
+        }
+      }
+
+      contenidoPantalla = resultado.toString();
+      operadorAnterior = "";
+      numeroAnterior = "";
     }
-    
-        
+  }
+
+  else {
+    contenidoPantalla = contenidoPantalla + valor;
+  }
+
+  pantalla.innerHTML = contenidoPantalla;
 }
